aa4eae33245fbad378ae9a24dd0a71adafae51ad
ddobi <qhdqhdekd261@naver.com>
2021-10-14 19:19:10 +0900

    로그인 페이지 추가

--- src/SessionRouter.tsx
+++ src/SessionRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/member/login/container/LoginPage';
 import MyPage from './pages/member/myPage/container/MyPage';
 import App from './App';
@@ -7,10 +7,12 @@ import App from './App';
 const SessionRouter = (): JSX.Element => {
   return (
     <Router>
-      <Route path="/login" component={LoginPage} />
-      <Route path="/myPage" component={MyPage} />
-      <Route path="/market" component={App} />
-      <Route path="/admin" component={App} />
+      <Switch>
+        <Route path="/login" component={LoginPage} />
+        <Route path="/myPage" component={MyPage} />
+        <Route path="/market" component={App} />
+        <Route path="/admin" component={App} />
+      </Switch>
     </Router>
   );
 };
