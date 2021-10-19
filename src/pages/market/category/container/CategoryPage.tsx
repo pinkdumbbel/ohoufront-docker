b2da178f1667a9cf1af41a2cb24ef1f7cedefa65
CHOI <tlacjd665@gmail.com>
2021-10-19 22:09:15 +0900

    common 폴더 삭제

--- src/pages/market/category/container/CategoryPage.tsx
+++ src/pages/market/category/container/CategoryPage.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Layout } from 'antd';
 import Header from '@/common/Header';
 import Footer from '@/common/Footer';
 import AppLayout from '@/common/AppLayout';
-//import AppLayout from '../../../../common/AppLayout';
 import './style.css';
 
 const CategoryPage: React.FC = () => {
   return (
-    <Layout className="layout">
+    <>
       <Header />
       <AppLayout />
       <Footer />
-    </Layout>
+    </>
   );
 };
 
