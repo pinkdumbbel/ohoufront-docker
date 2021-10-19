9fc6e4b43de6afae6ada0d935e8f73ca0d9c390c
CHOI <tlacjd665@gmail.com>
2021-10-19 22:27:13 +0900

    로그인 화면 로고 svg로 변경

--- src/pages/member/login/container/LoginPage.tsx
+++ src/pages/member/login/container/LoginPage.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Form, Input, Button } from 'antd';
 import 'antd/dist/antd.css';
-import './login.css'
+import './login.css';
 import { DownloadOutlined } from '@ant-design/icons';
+import OhousLogin from '@/common/svg/OhousLogin';
 
 const LoginPage: React.FC = () => {
   const [form] = Form.useForm();
@@ -17,51 +18,44 @@ const LoginPage: React.FC = () => {
 
   return (
     <>
-    <div className="login-page">
-      <div className="login-wrapper">
-        <img className="login-logo" src="logo_login.png" alt="login-logo" width="150" height="50"/>
-        <div className="login-form">
-          <Form
-            form={form}
-            name="basic"
-            size="large"
-          >
-            <Form.Item
-              name="username"
-            >
-              <Input size="large" placeholder="이메일" style={{ width: 300 }} />
-            </Form.Item>
+      <div className="login-page">
+        <div className="login-wrapper">
+          {/* <img className="login-logo" src="logo_login.png" alt="login-logo" width="150" height="50"/> */}
+          <OhousLogin />
+          <div className="login-form">
+            <Form form={form} name="basic" size="large">
+              <Form.Item name="username">
+                <Input size="large" placeholder="이메일" style={{ width: 300 }} />
+              </Form.Item>
 
-            <Form.Item
-              name="password"
+              <Form.Item name="password">
+                <Input.Password placeholder="비밀번호" style={{ width: 300 }} />
+              </Form.Item>
+            </Form>
+            <Button
+              style={{ marginTop: 20, marginBottom: 20, height: 50 }}
+              size="large"
+              block
+              type="primary"
+              htmlType="submit"
             >
-              <Input.Password placeholder="비밀번호" style={{ width: 300 }} />
-            </Form.Item>
-          </Form>
-          <Button style={{marginTop:20, marginBottom:20, height: 50}} size="large" block type="primary" htmlType="submit">
-            로그인
-          </Button>
-          <div className="login-form-sub">
-            <Button type="text">
-              비밀번호 재설정
+              로그인
             </Button>
-            <Button type="text">
-              회원가입
-            </Button>
+            <div className="login-form-sub">
+              <Button type="text">비밀번호 재설정</Button>
+              <Button type="text">회원가입</Button>
+            </div>
           </div>
-        </div>
-        <div className="sns-login">
-          <div className="sns-login-text">
-            SNS계정으로 간편 로그인/회원가입
-          </div>
-          <div className="sns-login-icon">
-            <Button shape="circle" icon={<DownloadOutlined />} size='large' />
-            <Button shape="circle" icon={<DownloadOutlined />} size='large' />
-            <Button shape="circle" icon={<DownloadOutlined />} size='large' />
+          <div className="sns-login">
+            <div className="sns-login-text">SNS계정으로 간편 로그인/회원가입</div>
+            <div className="sns-login-icon">
+              <Button shape="circle" icon={<DownloadOutlined />} size="large" />
+              <Button shape="circle" icon={<DownloadOutlined />} size="large" />
+              <Button shape="circle" icon={<DownloadOutlined />} size="large" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
     </>
   );
 };
