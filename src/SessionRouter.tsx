14d1f433d29ee91980aa4c204379f1be69a9af28
newddobi <qhdqhdekd261@naver.com>
2021-10-21 20:26:31 +0900

    회원가입 퍼블리싱

--- src/SessionRouter.tsx
+++ src/SessionRouter.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/member/login/container/LoginPage';
+import SignUpPage from './pages/member/signUp/container/SignUpPage';
 import MyPage from './pages/member/myPage/container/MyPage';
 import App from './App';
 import CategoryPage from './pages/market/category/container/CategoryPage';
@@ -10,6 +11,7 @@ const SessionRouter: React.FC = () => {
     <Router>
       <Switch>
         <Route path="/login" component={LoginPage} />
+        <Route path="/signUp" component={SignUpPage} />
         <Route path="/myPage" component={MyPage} />
         <Route path="/market" component={App} />
         <Route path="/admin" component={App} />
---
+++ src/pages/member/signUp/container/SignUpPage.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Form, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
+import './signUp.css';
+import OhousLogin from '@/common/svg/OhousLogin';
+
+const SignUpPage: React.FC = () => {
+  const [form] = Form.useForm();
+
+  return (
+    <>
+      <div className="signup-page">
+        <a href="/">
+          <OhousLogin />
+        </a>
+        <div className="signup-wrapper">
+          <h1 className="signup-title">회원가입</h1>
+          <div className="sns-wrapper">
+            <div className="sns-login-title">SNS계정으로 간편하게 회원가입</div>
+            <ul className="sns-list">
+              <li className="sns-item">
+                <Button
+                  style={{ backgroundColor: '#3b5998', color: '#ffffff' }}
+                  shape="circle"
+                  icon={<>F</>}
+                  size="large"
+                />
+              </li>
+              <li className="sns-item">
+                <Button style={{ backgroundColor: '#fef01b' }} shape="circle" icon={<>N</>} size="large" />
+              </li>
+              <li className="sns-item">
+                <Button
+                  style={{ backgroundColor: '#2DB400', color: '#ffffff' }}
+                  shape="circle"
+                  icon={<>K</>}
+                  size="large"
+                />
+              </li>
+            </ul>
+          </div>
+          <Form form={form} name="basic" size="large" labelCol={{ span: 6 }} wrapperCol={{ span: 16 }}>
+            <Form.Item label="이메일" name="email">
+              <Input size="large" placeholder="이메일" />
+            </Form.Item>
+            <Form.Item label="비밀번호" name="password" extra="영문, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.">
+              <Input placeholder="비밀번호" />
+            </Form.Item>
+            <Form.Item label="비밀번호 확인" name="passwordConfirm">
+              <Input placeholder="비밀번호 확인" />
+            </Form.Item>
+            <Form.Item label="닉네임" name="nickName" extra="다른 유저와 겹치지 않는 별명을 입력해주세요. (2~15자)">
+              <Input placeholder="별명 (2~15자)" />
+            </Form.Item>
+            <Button size="large" block type="primary" htmlType="submit">
+              회원가입하기
+            </Button>
+          </Form>
+          <p className="signup-bottom-message">
+            이미 아이디가 있으신가요?<a className="login-link">로그인</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SignUpPage;
