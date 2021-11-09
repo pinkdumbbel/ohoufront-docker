1e744ab952ec72a365baca84621bdabcd3354408
newddobi <qhdqhdekd261@naver.com>
2021-11-09 19:52:19 +0900

    소스정리

--- src/SessionRouter.tsx
+++ src/SessionRouter.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import LoginPage from './pages/member/login/container/LoginPage';
 import SignUpPage from './pages/member/signUp/container/SignUpPage';
 import MyPage from './pages/member/myPage/container/MyPage';
-import App from './App';
 import CategoryPage from './pages/market/category/container/CategoryPage';
 import ChangePasswordPage from './pages/member/changePassword/container/ChangePasswordPage';
 import SellingPage from './pages/market/selling/container/SellingPage';
@@ -18,8 +17,8 @@ const SessionRouter: React.FC = () => {
         <Route path="/signUp" render={(props) => <SignUpPage {...props} />} />
         <Route path="/main" component={CategoryPage} exact />
         <Route path="/myPage" component={MyPage} />
-        <Route path="/market" component={App} />
-        <Route path="/admin" component={App} />
+        {/* <Route path="/market" component={} /> */}
+        {/* <Route path="/admin" component={} /> */}
         <Route path="/changePassword" component={ChangePasswordPage} />
         <Route path="/selling" component={SellingPage} />
       </Switch>
