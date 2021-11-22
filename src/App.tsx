05bcc796cb0d5349b8dc593f912870f5c2ca8408
newddobi <qhdqhdekd261@naver.com>
2021-11-22 20:20:21 +0900

    404페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import AppRouter from './AppRouter';
-import { useSelector } from 'react-redux';
-import { getState } from '@/pages/member/login/state';
-import { RootState } from '@/store/store';
 
 const App: React.FC = () => {
-  // 로그인 성공했는지 관리
-  const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const certifyYn = useSelector((state: RootState) => getState(state).certifyYn);
-
-  useEffect(() => {
-    if (localStorage.getItem('certifyYn')) {
-      setIsLoggedIn(true);
-    } else {
-      setIsLoggedIn(false);
-    }
-    setInit(true);
-  }, [certifyYn]);
-
-  return <>{init ? <AppRouter isLoggedIn={isLoggedIn} /> : '초기화'}</>;
+  return <AppRouter />;
 };
 
 export default App;
